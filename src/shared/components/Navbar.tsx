44e47937810ed867df0ba81cfecd45f9c52be9b3
jelizavetavetaskova <s24vetajeli@venta.lv>
2026-04-17 18:57:13 +0300

    feat: connected data to Navbar

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,12 +1,72 @@
-const Navbar = () => {
+import type {Class, School, User} from "../types/database.ts";
+import {useEffect, useState} from "react";
+import {supabase} from "../../lib/supabase.ts";
+import {useAuth} from "../../features/auth/hooks/useAuth.ts";
+import {Link} from "react-router-dom";
+
+interface NavbarProps {
+    user: User | null;
+}
+
+const Navbar = ({user}: NavbarProps) => {
+    const [school, setSchool] = useState<School|null>(null);
+    const [stClass, setStClass] = useState<Class|null>(null);
+
+    const {signOut} = useAuth();
+
+    useEffect(() => {
+        const getSchool = async () => {
+            if (!user?.school_id) return;
+
+            const {data, error} = await supabase.from("schools").select("*").eq("school_id", user?.school_id).single();
+
+            if (error) {
+                console.log(error.message);
+                return;
+            }
+
+            setSchool(data);
+        }
+
+        const getClass = async () => {
+            if (!user?.class_id) return;
+
+            const {data, error} = await supabase.from("classes").select("*").eq("class_id", user?.class_id).single();
+
+            if (error) {
+                console.log(error.message);
+                return;
+            }
+
+            setStClass(data);
+        }
+        
+        getSchool();
+        getClass();
+    }, [user?.school_id, user?.class_id]);
+
     return (
-        <div className="w-full bg-gray-200 p-5 flex items-center">
-            <p className="text-left flex-1 text-xl font-semibold">Liepājas Liedaga vidusskola, 6.a klase</p>
-            <div className="flex mx-3">
-                <div className="w-3 h-3 rounded-full bg-green-500 my-auto mr-1"/>
-                <p>Online</p>
-            </div>
-            <p className="text-right">Jānis Bērziņš</p>
+        <div>
+            <p>MATHIX</p>
+
+            {user ?
+                <div>
+                    { user.role === "student" && school && stClass &&
+                        <p>{school.name + ", "}{stClass.number + "." + stClass.letter + " klase"}</p>
+                    }
+                    <div>
+                        <div/>
+                        <p>Online</p>
+                    </div>
+                    <p>{user.name} {user.surname}</p>
+                    <button onClick={signOut}>Iziet</button>
+                </div>
+                :
+                <div>
+                    <Link to="/login">Autorizēties</Link> | <Link to="/register">Reģistrēties</Link>
+                </div>
+            }
+
         </div>
     )
 }
