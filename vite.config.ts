9e38afb85c025e96e80db1da64f63296cb464def
jelizavetavetaskova <s24vetajeli@venta.lv>
2026-05-13 15:45:16 +0300

    feature: basic configuration of PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: "autoUpdate",
+          devOptions: {
+            enabled: true
+          },
+        manifest: {
+          name: "SKAITRIXA",
+          short_name: "SKAITRIXA",
+          description: "Galvas rēķinu treniņu platforma",
+          theme_color: "#6D28D9",
+          background_color: "#F8F9FA",
+          display: "standalone",
+          icons: [
+              {
+                  src: "android-chrome-192x192.png",
+                  sizes: "192x192",
+                  type: "image/png"
+              },
+              {
+                  src: "android-chrome-512x512.png",
+                  sizes: "512x512",
+                  type: "image/png"
+              }
+          ]
+        }
+      })
+  ],
 })
