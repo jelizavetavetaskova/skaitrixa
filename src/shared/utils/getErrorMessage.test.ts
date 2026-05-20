d93ab9d1d7594b033fc6eee588ab57e4f3fdb44d
jelizavetavetaskova <s24vetajeli@venta.lv>
2026-05-20 13:28:06 +0300

    testing: error message tests

--- src/shared/utils/getErrorMessage.test.ts
+++ src/shared/utils/getErrorMessage.test.ts
@@ -1,9 +1,19 @@
 import {describe, expect, test} from "vitest";
 import {getErrorMessage} from "./getErrorMessage.ts";
 
-describe('getErrorMessage', () => {
-    test('returns message from Error instance', () => {
+describe("getErrorMessage", () => {
+    test("returns message from Error instance", () => {
         const err = new Error("test error");
         expect(getErrorMessage(err)).toBe("test error");
-    })
+    });
+
+    test("returns message from other object with message property", () => {
+        const err = {id: 1, message: "test error"};
+        expect(getErrorMessage(err)).toBe("test error")
+    });
+
+    test("unknown error", () => {
+        const err = 123;
+        expect(getErrorMessage(err)).toBe("Nezināmā kļūda");
+    });
 })
